c2c43f529522bdd9fe8fe94ed4c04c192737f4dc
ChajouiW <mohammedchajoui@gmail.com>
2026-07-30 15:35:38 +0100

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        long sum = 0;
+        int n = nums.size();
+        n = n * (n + 1) / 2;
+        for (int i = 0; i < nums.size(); i++)
+            sum += nums[i];
+        return n - sum;
+    }
+};
